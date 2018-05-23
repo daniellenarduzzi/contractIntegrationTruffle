a08b1e88c1c10eac7e0814c2c7584fefb04b2d3a
Castor <daniellenarduzzi@gmail.com>
2018-05-23 11:18:36 -0300

    ingresando parametros e intrgracion con ganache

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 var NotarizeTx = artifacts.require("NotarizeTx");
 
 module.exports = function(deployer) {
-  deployer.deploy(NotarizeTx);
+  const _buyer =
+  const _seller = 
+  const _id =
+  const _date =
+  const _value =
+  const _hash =
+  const _status =
+  const _shipping =
+  deployer.deploy(NotarizeTx, buyer, seller, id, date, value, hash, status, shipping)
 };
